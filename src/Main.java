c5e22731ffbc7496addb937a0ffcce37d07dbb64
Shinbiraha <diederick.prosman@icloud.com>
2025-05-09 10:52:16 +0200

    Added a play again function

--- src/Main.java
+++ src/Main.java
@@ -7,32 +7,44 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         String[] choices = {"Rock", "Paper", "Scissors"};
+        String playAgain = "Yes";
 
-        //let the player choose a number from 0 to 2
-        System.out.println("Choose:\n0 (Rock)\n1 (Paper)\n2 (Scissors)");
+        do {
 
-        // check of invoer een geheel getal is
-        if (!scanner.hasNextInt()) {
-            System.out.println("Invalid input! Please enter a number: 0 (Rock), 1 (Paper), or 2 (Scissors).");
-            return;
-        }
+            //let the player choose a number from 0 to 2
+            System.out.println("Choose:\n0 (Rock)\n1 (Paper)\n2 (Scissors)");
 
-        int playerChoice = scanner.nextInt();
+            // check if the input is a full number
+            if (!scanner.hasNextInt()) {
+                System.out.println("Invalid input! Please enter a number: 0 (Rock), 1 (Paper), or 2 (Scissors).");
+                scanner.next();
+                continue;
+            }
 
-        //validate player input
-        if (playerChoice < 0 || playerChoice > 2) {
-            System.out.println("Invalid Choice! Please choose 0, 1, 2.");
-            return;
-        }
+            int playerChoice = scanner.nextInt();
 
-        //let the computer choose a number from 0 to 2.9999
-        int computerChoice = (int) (Math.random() * 3);
+            //validate player input
+            if (playerChoice < 0 || playerChoice > 2) {
+                System.out.println("Invalid Choice! Please choose 0, 1, 2.");
+                continue;
+            }
 
-        //print out both player and computer inputs
-        System.out.println("Your choice: " + choices[playerChoice]);
-        System.out.println("Computer Choice: " + choices[computerChoice]);
+            //let the computer choose a number from 0 to 2.9999
+            int computerChoice = (int) (Math.random() * 3);
 
-        determineWinner(computerChoice, playerChoice);
+            //print out both player and computer inputs
+            System.out.println("Your choice: " + choices[playerChoice]);
+            System.out.println("Computer Choice: " + choices[computerChoice]);
+
+            determineWinner(computerChoice, playerChoice);
+
+            System.out.println("\nDo you want to play again? (yes/no)");
+            playAgain = scanner.next();
+
+        } while (playAgain.equalsIgnoreCase("yes"));
+
+        // Afsluiten bericht
+        System.out.println("Thanks for playing!");
     }
 
     //choose a winner using else if statements
